8850cf0f3828e13c3dc3ba0fd375a94e5603efc4
CAIO LOPES DURAES <caio.lduraes@senacsp.edu.br>
2025-05-13 22:21:13 -0300

    Atualizar

---
+++ database.js
@@ -0,0 +1,42 @@
+const mongooge = require('mongoose')
+
+const url = 'mongodb+srv://admin:123Senac@cluster0.lfstp.mongodb.net/
+
+let conectado = false
+const conectar = async () => {
+    if (!conectado) {
+
+   try {
+    await mongoose.connect(url)
+    conectado = trueconsole.log("MongoDB Conectado")
+    return true
+   } catch (error) {
+    if (error.code = 8000) {
+        console.log("Erro de autenticacao")
+    } else {
+        console.log(error)
+    }
+    return false
+   }
+}
+}
+
+const desconctar = async () => {
+    const desconectar = async () => {
+       
+        if (conectado) {
+                   
+            try {
+                await mongoose.disconnect(url) 
+                conectado = false 
+                console.log("MongoDB desconectado")
+                return true 
+            } catch (error) {
+                console.log(error)
+                return false
+            }
+        }
+    }
+    
+    module.exports = { conectar, desconectar }
+}
